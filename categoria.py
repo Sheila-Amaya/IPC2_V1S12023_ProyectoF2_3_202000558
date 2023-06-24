4e68f148addbd48229a9b1693b607840fdf06a64
Sheila-Amaya <2851326790117@ingenieria.usac.edu.gt>
2023-06-24 01:56:51 -0600

    ...
    ...

--- categoria.py
+++
@@ -1,4 +0,0 @@
-class Categoria:
-    def __init__(self,nombre,pelicula):
-        self.nombre = nombre
-        self.pelicula = pelicula  #lista
--- sala.py
+++
@@ -1,4 +0,0 @@
-class Sala:
-    def __init__(self,num,asientos):
-        self.num = num
-        self.asientos = asientos
